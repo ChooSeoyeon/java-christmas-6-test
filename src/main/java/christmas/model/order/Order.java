8a7858df989eb6acf400ea4ad3d20530d21c58c0
Choo <choo000407@naver.com>
2023-12-15 16:18:15 +0900

    feat(Order): 메뉴(PK), 개수로 주문을 생성한다

--- src/main/java/christmas/model/order/Order.java
+++ src/main/java/christmas/model/order/Order.java
@@ -1,4 +1,28 @@
 package christmas.model.order;
 
+import christmas.model.order.enums.Menu;
+
 public class Order {
+    private final Menu menu;
+    private final int count;
+
+    public Order(Menu menu, int count) {
+        this.menu = menu;
+        this.count = count;
+        validate();
+    }
+
+    private void validate() {
+        if (count < 1) {
+            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
