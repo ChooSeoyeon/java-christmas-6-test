af18a5f67ad832d8d394a8c9349856f172036d53
Choo <choo000407@naver.com>
2023-12-15 16:01:06 +0900

    feat(Calendar): 방문 날짜로 달력을 생성한다

--- src/main/java/christmas/model/event/EventCalendar.java
+++ src/main/java/christmas/model/event/EventCalendar.java
@@ -1,4 +1,20 @@
 package christmas.model.event;
 
+import java.time.LocalDate;
+
 public class EventCalendar {
+    private LocalDate visitedDate;
+
+    public EventCalendar(LocalDate visitedDate) {
+        this.visitedDate = visitedDate;
+    }
+
+    public static EventCalendar of(int date) {
+        try {
+            LocalDate visitedDate = LocalDate.of(2023, 12, date);
+            return new EventCalendar(visitedDate);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
 }
