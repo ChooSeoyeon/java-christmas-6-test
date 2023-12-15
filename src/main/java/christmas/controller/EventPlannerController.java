f56822fab5d4b3c6837f2a9fe7b5717047c8c010
Choo <choo000407@naver.com>
2023-12-15 14:52:09 +0900

    feat(Controller): 에러 메시지를 관리한다

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -1,4 +1,40 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.function.Supplier;
+
 public class EventPlannerController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public EventPlannerController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+    }
+
+    private <T> T repeatUntilSuccessWithReturn(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void repeatUntilSuccess(Runnable action) {
+        while (true) {
+            try {
+                action.run();
+                return;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
 }
