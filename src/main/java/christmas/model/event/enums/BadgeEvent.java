f52e23ce3ecf7e0cde29daead89c07c30e64f73f
Choo <choo000407@naver.com>
2023-12-15 17:06:44 +0900

    feaet(event): 증정 이벤트와 배지 이벤트를 정의한다

--- src/main/java/christmas/model/event/enums/BadgeEvent.java
+++ src/main/java/christmas/model/event/enums/BadgeEvent.java
@@ -1,4 +1,33 @@
 package christmas.model.event.enums;
 
 public enum BadgeEvent {
+    SANTA("산타", 20000),
+    TREE("트리", 10000),
+    STAR("별", 5000),
+    NONE("없음", 0);
+
+    private final String name;
+    private final int threshold;
+
+    BadgeEvent(String name, int threshold) {
+        this.name = name;
+        this.threshold = threshold;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isEligible(int totalBenefit) {
+        return totalBenefit >= threshold;
+    }
+
+    public static BadgeEvent findEligibleBadge(int totalBenefit) {
+        for (BadgeEvent badge : BadgeEvent.values()) {
+            if (badge.isEligible(totalBenefit)) {
+                return badge;
+            }
+        }
+        return NONE;
+    }
 }
--- src/main/java/christmas/model/event/enums/GiftEvent.java
+++ src/main/java/christmas/model/event/enums/GiftEvent.java
@@ -1,4 +1,37 @@
 package christmas.model.event.enums;
 
 public enum GiftEvent {
+    CHAMPAGNE_GIFT("샴페인", 1, 120000),
+    NONE("없음", 0, 0);
+
+    private final String giftMenu;
+    private final int quantity;
+    private final int threshold;
+
+    GiftEvent(String giftMenu, int quantity, int threshold) {
+        this.giftMenu = giftMenu;
+        this.quantity = quantity;
+        this.threshold = threshold;
+    }
+
+    public String getGiftMenu() {
+        return giftMenu;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public boolean isEligible(int totalAmount) {
+        return totalAmount >= threshold;
+    }
+
+    public static GiftEvent findEligibleEvent(int totalAmount) {
+        for (GiftEvent event : GiftEvent.values()) {
+            if (event.isEligible(totalAmount)) {
+                return event;
+            }
+        }
+        return NONE;
+    }
 }
