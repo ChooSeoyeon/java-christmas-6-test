96e4ee3c92c95f0180a2f3d5fe661212a3d71b77
Choo <choo000407@naver.com>
2023-12-15 16:05:22 +0900

    feat(Calendar): 방문날짜가 조건을 만족하는지 확인한다

--- src/main/java/christmas/model/event/EventCalendar.java
+++ src/main/java/christmas/model/event/EventCalendar.java
@@ -1,8 +1,10 @@
 package christmas.model.event;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class EventCalendar {
+    private static final int[] SPECIAL_DAYS = {3, 10, 17, 24, 25, 31};
     private LocalDate visitedDate;
 
     public EventCalendar(LocalDate visitedDate) {
@@ -16,5 +18,27 @@ public class EventCalendar {
         } catch (Exception e) {
             throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
+    }
+
+    public boolean isSpecialDay() {
+        for (int specialDay : SPECIAL_DAYS) {
+            if (visitedDate.getDayOfMonth() == specialDay) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isWeekend() {
+        return visitedDate.getDayOfWeek() == DayOfWeek.FRIDAY
+                || visitedDate.getDayOfWeek() == DayOfWeek.SATURDAY;
+    }
+
+    public boolean isWeekday() {
+        return !isWeekend();
+    }
+
+    public boolean isBeforeChristmasDay() {
+        return visitedDate.getDayOfMonth() <= 25;
     }
 }
