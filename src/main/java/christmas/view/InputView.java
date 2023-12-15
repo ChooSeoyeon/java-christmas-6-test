cc68dce10c0f6c9042567d8bd76b40d504c70f0a
Choo <choo000407@naver.com>
2023-12-15 14:55:36 +0900

    feat(InputView): 방문 날짜를 입력받는다

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,19 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public int readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String inputDate = Console.readLine();
+        return parseInputDateToInt(inputDate);
+    }
+
+    private int parseInputDateToInt(String inputDate) {
+        try {
+            return Integer.parseInt(inputDate);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("방문 날짜는 숫자만 입력 가능합니다.");
+        }
+    }
 }
