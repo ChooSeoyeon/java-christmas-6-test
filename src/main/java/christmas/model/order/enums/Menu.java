cb4d9133ff47b4c89167aaf56aeaddbcf06343f2
Choo <choo000407@naver.com>
2023-12-15 16:09:02 +0900

    feat(Menu): 메뉴 타입과 메뉴를 정의한다

--- src/main/java/christmas/model/order/enums/Menu.java
+++ src/main/java/christmas/model/order/enums/Menu.java
@@ -1,4 +1,69 @@
 package christmas.model.order.enums;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Menu {
+    MUSHROOM_SOUP("양송이수프", 6000, MenuType.APPETIZER),
+    TAPAS("타파스", 5500, MenuType.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8000, MenuType.APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55000, MenuType.MAIN),
+    BBQ_RIBS("바비큐립", 54000, MenuType.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35000, MenuType.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, MenuType.MAIN),
+    CHOCOLATE_CAKE("초코케이크", 15000, MenuType.DESSERT),
+    ICE_CREAM("아이스크림", 5000, MenuType.DESSERT),
+    ZERO_COLA("제로콜라", 3000, MenuType.DRINK),
+    RED_WINE("레드와인", 60000, MenuType.DRINK),
+    CHAMPAGNE("샴페인", 25000, MenuType.DRINK);
+
+    private final String name;
+    private final int price;
+    private final MenuType type;
+
+    Menu(String name, int price, MenuType type) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public MenuType getType() {
+        return type;
+    }
+
+    public static boolean exists(String name) {
+        for (Menu menu : Menu.values()) {
+            if (menu.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static MenuType findMenuTypeByName(String name) {
+        for (Menu menu : Menu.values()) {
+            if (menu.getName().equals(name)) {
+                return menu.getType();
+            }
+        }
+        return null;
+    }
+
+    public static List<Menu> findMenusByType(MenuType type) {
+        List<Menu> menus = new ArrayList<>();
+        for (Menu menu : Menu.values()) {
+            if (menu.getType() == type) {
+                menus.add(menu);
+            }
+        }
+        return menus;
+    }
 }
--- src/main/java/christmas/model/order/enums/MenuType.java
+++ src/main/java/christmas/model/order/enums/MenuType.java
@@ -1,4 +1,18 @@
 package christmas.model.order.enums;
 
 public enum MenuType {
+    APPETIZER("애피타이저"),
+    MAIN("메인"),
+    DESSERT("디저트"),
+    DRINK("음료");
+
+    private final String name;
+
+    MenuType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
